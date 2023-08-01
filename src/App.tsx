963100bf7d088ab7aa768fa502a42bb86e4fccef
Thanmai_Aqarios <>
2023-08-01 23:16:42 +0200

    style-fix for mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react';
-import WaveSurfer from 'wavesurfer.js';
+import React, { useEffect, useState } from 'react';
 import styles from './App.module.css';
 
 interface ServerMessage {
@@ -12,8 +11,6 @@ const App: React.FC = () => {
   const [socket, setSocket] = useState<WebSocket | null>(null);
   const [recorder, setRecorder] = useState<any | null>(null);
   const [serverMessage, setServerMessage] = useState<ServerMessage | null>(null);
-
-  const serverWaveSurfer = useRef<WaveSurfer | null>(null);
 
   // Establish websocket connection
   useEffect(() => {
@@ -31,29 +28,35 @@ const App: React.FC = () => {
       if (data.response_audio) {
         let audio = new Audio('data:audio/wav;base64,' + data.response_audio);
 
-        fetch('data:audio/wav;base64,' + data.response_audio)
-          .then(response => response.blob())
-          .then(audioBlob => {
-            serverWaveSurfer.current?.loadBlob(audioBlob);
-          });
-
         audio.onerror = function() {
-          console.log('Audio playback error');
+          if (audio.error) {
+            switch(audio.error.code) {
+              case audio.error.MEDIA_ERR_ABORTED:
+                console.log('You aborted the playback.');
+                break;
+              case audio.error.MEDIA_ERR_NETWORK:
+                console.log('A network error caused the audio download to fail.');
+                break;
+              case audio.error.MEDIA_ERR_DECODE:
+                console.log('The audio playback was aborted due to a corruption problem or because the media used features your browser did not support.');
+                break;
+              case audio.error.MEDIA_ERR_SRC_NOT_SUPPORTED:
+                console.log('The audio could not be loaded, either because the server or network failed or because the format is not supported.');
+                break;
+              default:
+                console.log('An unknown error occurred.');
+                break;
+            }
+          } else {
+            console.log('audio.error is null');
+          }
         };
-
         audio.oncanplay = function() {
             console.log('Audio is able to start playing now');
             audio.play();
         };
       }
     };
-
-    serverWaveSurfer.current = WaveSurfer.create({
-      container: '#server-waveform',
-      waveColor: 'violet',
-      progressColor: 'purple'
-    });
-
   }, []);
 
   const startRecording = () => {
@@ -98,12 +101,7 @@ const App: React.FC = () => {
     <div className={styles.app}>
       <h2 className={styles.title}>Digitar.ai</h2>
       <div className={styles.section}>
-        <h3>Your Query:</h3>
-        <textarea className={styles.textBox} readOnly value={serverMessage?.transcribed_text || ''} />
-      </div>
-      <div className={styles.section}>
         <h3>Digitar Response:</h3>
-        <div id="server-waveform"></div>
         <textarea className={styles.textBox} readOnly value={serverMessage?.response_text || ''} />
       </div>
       <button className={styles.button} onClick={startRecording}>Start Talking</button>
